6ffcf2c333be270bcbec6dfca5a7513a86657fd0
pirminj <43973067+pirminj@users.noreply.github.com>
2024-06-29 16:53:20 +0200

    fix early init
    Had problems at startup

--- early-init.el
+++ early-init.el
@@ -1,15 +1,15 @@
-(setq gc-cons-threshold 10000000)
-(setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
-(setq native-comp-async-report-warnings-errors 'silent)
+;;; early-init.el -*- lexical-binding: t; -*-
 
+(setq gc-cons-threshold most-positive-fixnum)
 (setq inhibit-startup-echo-area-message (user-login-name))
 (setq default-frame-alist '((fullscreen . maximized)
                             (background-color . "#000000")
                             (ns-appearance . dark)
-                            (ns-transparent-titlebar . t)))
+                            (ns-transparent-titlebar . t)
+			    (menu-bar-mode . 0)
+			    (scroll-bar-mode . 0)
+			    (tool-bar-mode . 0)))
 
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
-
